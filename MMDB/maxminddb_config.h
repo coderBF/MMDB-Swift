ac36575e3790e45b32a002260d2695fd866dd570
Zhuhao Wang <zhuhaow@gmail.com>
2016-07-06 07:39:22 +0800

    Add necessary header

--- MMDB/maxminddb_config.h
+++ MMDB/maxminddb_config.h
@@ -1,6 +1,8 @@
 #ifndef MAXMINDDB_CONFIG_H
 #define MAXMINDDB_CONFIG_H
 
+#import "TargetConditionals.h"
+
 #ifndef MMDB_UINT128_USING_MODE
 /* Define as 1 if we we use unsigned int __atribute__ ((__mode__(TI))) for uint128 values */
 #define MMDB_UINT128_USING_MODE 0
@@ -11,8 +13,7 @@
 #if TARGET_OS_IPHONE
 #define MMDB_UINT128_IS_BYTE_ARRAY 1
 #else
-#undef MMDB_UINT128_IS_BYTE_ARRAY
+#define MMDB_UINT128_IS_BYTE_ARRAY 0
 #endif
 #endif
-
 #endif                          /* MAXMINDDB_CONFIG_H */
